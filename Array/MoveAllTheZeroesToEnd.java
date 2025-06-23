09c1d3b07cc1b9d87741f3b90c610137d6c987d5
uttamsingh888 <uttamsingh888@gmail.com>
2025-06-23 14:58:12 +0530

    added more sol moveallthezeroes

--- Array/MoveAllTheZeroesToEnd.java
+++ Array/MoveAllTheZeroesToEnd.java
@@ -1,44 +1,88 @@
 /*
-* 283. Move Zeroes (Easy)
-*
-* Given an integer array nums, move all 0's to the end of it,
-* while maintaining the relative order of the non-zero elements.
-*
-* Note that you must do this in-place without making a copy of the array.
-* */
+ * 283. Move Zeroes (Easy)
+ * https://leetcode.com/problems/move-zeroes/
+ * Given an integer array nums, move all 0's to the end of it,
+ * while maintaining the relative order of the non-zero elements.
+ *
+ * Note that you must do this in-place without making a copy of the array.
+ * */
 public class MoveAllTheZeroesToEnd {
 
     public static void main(String args[]) {
-
         int arr1[] = {0, 1, 5, 0, 1, 1, 5, 0};
         int arr2[] = {0, 0, 0, 0, 0, 0, 1, 5};
         int arr3[] = {0, 1, 5, 0, 1, 1, 5, 0};
-        twoPointer(arr2);
+        int arr4[] = {0, 0, 5, 0, 1, 0, 2, 5, 0};
+        int arr5[] = {0, 0, 5, 0, 1, 0, 2, 5, 0};
+
         bruteForce(arr1);
-        extraSpace(arr3);
+        extraSpace(arr2);
+        twoPointerWhile(arr3);
+        twoPointerFor(arr4);
+        replaceZero(arr5);
     }
 
-    private static void twoPointer(int[] arr) {
-        int indexPointer1 = 0;
-        int indexPointer2 = 1;
+    private static void replaceZero(int[] nums) {
+        int indexCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[indexCount] = nums[i];
+                indexCount++;
+            }
+        }
+        for (int i = indexCount; i < nums.length; i++) {
+            nums[i] = 0;
+        }
 
-        while(indexPointer2 != arr.length) {
-            if(arr[indexPointer1] == 0) {
-                if(arr[indexPointer2] != 0) {
-                    //swap
-                    int temp = arr[indexPointer2];
-                    arr[indexPointer2] = arr[indexPointer1];
-                    arr[indexPointer1] = temp;
-                    indexPointer1++;
-                }
-                indexPointer2++;
+        System.out.print("\nreplaceZero: ");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    private static void twoPointerFor(int[] nums) {
+        int indexPointer = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0 && indexPointer >= 0 && indexPointer != i) {
+                nums[indexPointer] = nums[i];
+                nums[i] = 0;
+                indexPointer++;
+            } else if (nums[i] == 0 && indexPointer == -1) {
+                indexPointer = i;
             }
         }
 
-        System.out.println("Two Pointer:");
+        System.out.print("\nTwoPointerFor: ");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    private static void twoPointerWhile(int[] nums) {
+        int indexPointer1 = 0;
+        int indexPointer2 = 1;
+
+        while (indexPointer2 < nums.length) {
+            if (nums[indexPointer1] == 0) {
+                if (nums[indexPointer2] != 0) {
+                    // swap
+                    int temp = nums[indexPointer1];
+                    nums[indexPointer1] = nums[indexPointer2];
+                    nums[indexPointer2] = temp;
+                    indexPointer1++;
+                }
+                indexPointer2++;
+            } else {
+                indexPointer1++;
+                if (indexPointer2 <= indexPointer1) {
+                    indexPointer2 = indexPointer1 + 1;
+                }
+            }
+        }
+
+        System.out.print("\nTwoPointerWhile: ");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
         }
     }
 
@@ -52,9 +96,9 @@ public class MoveAllTheZeroesToEnd {
             }
         }
 
-        System.out.println("Extra Space:");
+        System.out.print("\nExtraSpace: ");
         for (int i = 0; i < duplicateArr.length; i++) {
-            System.out.print(duplicateArr[i]+ " ");
+            System.out.print(duplicateArr[i] + " ");
         }
     }
 
@@ -64,7 +108,7 @@ public class MoveAllTheZeroesToEnd {
                 moveElementToEnd(i, arr);
             }
         }
-        System.out.println("Bruteforce:");
+        System.out.print("\nBruteforce: ");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
